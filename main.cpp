ad390c49f43a40969ff1dcb406fa80ee54dc170c
Peter Mackenzie-Helnwein <mackheln@gmail.com>
2019-06-13 22:34:20 -0700

    Fighting the battle with style sheets

--- main.cpp
+++ main.cpp
@@ -1,11 +1,51 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFile>
+
+QString openStyleFiles()
+{
+    QString ret;
+    QFile mainStyleFile(":/resources/styleSheets/TInFStyles.qss");
+
+#ifdef Q_OS_WIN
+    QFile appendedStyle(":/resources/styleSheets/TInFWin.qss");
+#endif
+
+#ifdef Q_OS_MACOS
+    QFile appendedStyle(":/resources/styleSheets/TInFMac.qss");
+#endif
+
+#ifdef Q_OS_LINUX
+    QFile appendedStyle(":/resources/styleSheets/TInFLinux.qss");
+#endif
+
+    if (!mainStyleFile.open(QFile::ReadOnly))
+    {
+        return ret;
+    }
+
+    if (!appendedStyle.open(QFile::ReadOnly))
+    {
+        return ret;
+    }
+
+    ret = ret.append(mainStyleFile.readAll());
+    ret = ret.append(appendedStyle.readAll());
+
+    mainStyleFile.close();
+    appendedStyle.close();
+
+    return ret;
+}
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
     MainWindow w;
+    QApplication::setWindowIcon(QIcon(":/resources/NHERI-TInF-icon.icns"));
     w.show();
 
-    return a.exec();
+    app.setStyleSheet(openStyleFiles());
+
+    return app.exec();
 }
